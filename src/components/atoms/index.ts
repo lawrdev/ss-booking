2277a3874e8842f493cdb11fd69468619b9d6501
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-20 23:23:57 +0100

    file added

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,7 @@
+export * from "./logo";
+export * from "./container";
+export * from "./text";
+export * from "./input";
+export * from "./select";
+export * from "./modal";
+export * from "./icons";
---
+++ src/components/organisms/index.ts
@@ -0,0 +1,4 @@
+export * from "./header";
+export * from "./forms";
+export * from "./hero";
+export * from "./footer";
