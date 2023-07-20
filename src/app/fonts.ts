a4521d17eb2e6b3ee0ec889a4a60bb4fbdfebb20
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-20 23:22:11 +0100

    style: adds fonts and styles

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import { Raleway } from "next/font/google";
+
+export const raleway = Raleway({
+  weight: ["500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  variable: "--font-raleway",
+});
